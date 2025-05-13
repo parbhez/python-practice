00d110adfcb616d335dbb869697d45cf0ff04127
parbhez <masud.affb@gmail.com>
2025-05-13 22:24:33 +0600

    added discount function

--- data-validation.py
+++ data-validation.py
@@ -1,8 +1,9 @@
 class Product:
-    def __init__(self, name: str, price: float, qty: int):
+    def __init__(self, name: str, price: float, qty: int, discount = 0):
         self.name = self.validate_name(name)
         self.price = self.validate_price(price)
         self.qty = self.validate_qty(qty)
+        self.discount = self.validate_discount(discount)
 
 
     def validate_name(self, name):
@@ -30,11 +31,26 @@ class Product:
                  raise ValueError("Qty must be positive")
             return qty
     
+    def validate_discount(self, discount):
+        try:
+            discount = float(discount)
+        except(ValueError, TypeError):
+             raise TypeError("Discount must be float")
+        
+        if discount < 0 or discount > 100:
+             raise ValueError("Discount can not be negative number")
+        return discount
+        
 
     def priceCalculate(self):
         return self.price * self.qty
+    
+    def getDiscount(self, price, discount):
+        net_amount = price - (price * discount / 100)
+        return net_amount
+    
 
 
 # Example usage
-p = Product("Apple", "10", "10")
-print(p.priceCalculate())  # Output: 1000.0
+p = Product("Apple", 100, 10, 10) # name, price, qty, discount
+print(p.getDiscount(p.price, p.discount))  # Output: 1000.0

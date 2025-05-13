9a89679c0d5bff3211882736e52b442a23602927
parbhez <masud.affb@gmail.com>
2025-05-13 23:14:33 +0600

    added python OOP DIct and doc method magic method

--- data-validator/product.py
+++ data-validator/product.py
@@ -1,6 +1,15 @@
 from validator import validate_string, validate_float, validate_int, validate_discount
 
 class Product:
+    """
+    This is a Product class that handles basic product information.
+
+    Features:
+        - Validates input data such as name, price, quantity, and discount.
+        - Calculates total price.
+        - Applies discount and returns net price after discount.
+    """
+
     def __init__(self, name: str, price: float, qty: int, discount=0):
         self.name = validate_string(name, "Name")
         self.price = validate_float(price, "Price")
@@ -15,6 +24,10 @@ class Product:
         return self.priceCalculate() - discount_amount
 
 
-p = Product("Apple", "10", "5", 10) # name, price, qty, discount
+# Example usage
+p = Product("Apple", "10", "5", 10)  # name, price, qty, discount
 print("Total:", p.priceCalculate())         # 50
 print("Net after Discount:", p.getNetAmount())  # 45
+
+#print(Product.__dict__)
+#print(Product.__doc__)

573b3d921a3c99653b08435f0d70a66db96bfda1
parbhez <masud.affb@gmail.com>
2025-05-13 22:51:02 +0600

    refactor code

---
+++ data-validator/product.py
@@ -0,0 +1,20 @@
+from validator import validate_string, validate_float, validate_int, validate_discount
+
+class Product:
+    def __init__(self, name: str, price: float, qty: int, discount=0):
+        self.name = validate_string(name, "Name")
+        self.price = validate_float(price, "Price")
+        self.qty = validate_int(qty, "Quantity")
+        self.discount = validate_discount(discount, "Discount")
+
+    def priceCalculate(self):
+        return self.price * self.qty
+
+    def getNetAmount(self):
+        discount_amount = (self.price * self.qty) * (self.discount / 100)
+        return self.priceCalculate() - discount_amount
+
+
+p = Product("Apple", "10", "5", 10) # name, price, qty, discount
+print("Total:", p.priceCalculate())         # 50
+print("Net after Discount:", p.getNetAmount())  # 45
---
+++ data-validator/validator.py
@@ -0,0 +1,33 @@
+def validate_string(value, field_name="Value"):
+    if not isinstance(value, str):
+        raise TypeError(f"{field_name} must be a string")
+    return value
+
+def validate_float(value, field_name="Value"):
+    try:
+        value = float(value)
+    except (ValueError, TypeError):
+        raise TypeError(f"{field_name} must be a float")
+    if value < 0:
+        raise ValueError(f"{field_name} must be positive")
+    return value
+
+def validate_int(value, field_name="Value"):
+    try:
+        value = int(value)
+    except (ValueError, TypeError):
+        raise TypeError(f"{field_name} must be an integer")
+    if value < 0:
+        raise ValueError(f"{field_name} must be positive")
+    return value
+
+def validate_discount(value, field_name="Discount"):
+    try:
+        value = float(value)
+    except (ValueError, TypeError):
+        raise TypeError(f"{field_name} must be a float")
+    
+    if not (0 < value <= 100):
+        raise ValueError(f"{field_name} must be greater than 0 and less than or equal to 100")
+    
+    return value

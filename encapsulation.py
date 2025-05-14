44e9fd0986cafa58e766d974b47b71375e055fbb
Mashud Parbhez <34704149+parbhez@users.noreply.github.com>
2025-05-14 15:54:36 +0600

    added another class and access this class from parent object

--- encapsulation.py
+++ encapsulation.py
@@ -4,11 +4,11 @@ class BankAccount:
         self._balance = balance #protected attribute
         self.__pin = 1234 #private attribute
 
-
+    @property
     def balance(self):
         return self._balance
 
-
+    @balance.setter
     def balance(self, value):
         if value < 0:
             print("Value can not be negative")
@@ -27,10 +27,17 @@ class BankAccount:
 
 
 
+class BankManager:
+    def show_account_info(self, account):
+        print(f"Owner: {account.owner}")  # ✅ accessing public attribute
+        print(f"Balance: {account.balance}")  # ✅ accessing via property getter
+
+
+
 # ✅ Object creation and usage
 account = BankAccount("Masud", 1000)
 
-# Accessing public and protected attributes (not recommended directly)
+#Accessing public and protected attributes (not recommended directly)
 print("Owner:", account.owner)
 print("Balance (via getter):", account.balance)
 
@@ -43,3 +50,9 @@ print("Current PIN:", account.get_pin())
 print(account.set_pin(1234, 4321))  # Correct old pin
 print("Updated PIN:", account.get_pin())
 print(account.set_pin(1111, 9999))  # Wrong old pin
+
+
+# ✅ Object creation and usage
+# Use from another class
+manager = BankManager()
+manager.show_account_info(account)
